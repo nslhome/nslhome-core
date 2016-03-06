d45b10e7a01270591a55c8ec4e46e438f86bf6b0
Nick Largent <nicklargent@gmail.com>
2016-03-06 12:27:39 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+exports = module.exports = nslhome_core = {};
+
+nslhome_core.Logger = require('./lib/logger');
+nslhome_core.DeviceMangager = require('./lib/device-manager');
+nslhome_core.Provider = require('./lib/provider');
+
+nslhome_core.provider = function() {
+    return new nslhome_core.Provider();
+};
+
+nslhome_core.logger = function(source, config) {
+    return new nslhome_core.Logger(source, config);
+}
---
+++ test.js
@@ -0,0 +1,49 @@
+require('env2')('.env');
+var provider = require('./index').provider();
+var deviceManager = require('./index').DeviceMangager;
+var logger = require('./index').logger('test', {mongoOutput: false});
+
+
+// controller
+
+deviceManager.watchDevice('test:1', function(update) {
+    logger.info("device update", update);
+});
+
+
+
+// provider
+
+provider.initialize("test-provider", "test1", function(err, config) {
+    if (err) {
+        logger.error(err);
+        process.exit(1);
+    }
+
+    logger.info("provider ready");
+    logger.info("config", config);
+
+    var triggerState = false;
+    var body = {
+        'id': 'test:1',
+        'name': 'Test Device 1',
+        'type': "binarysensor",
+        'sensortype': "virtual",
+        'triggerState': triggerState
+    };
+    provider.send({name: 'device', body: body});
+
+    setInterval(function() {
+        triggerState = !triggerState;
+        var update = {
+            id: 'test:1',
+            triggerState: triggerState
+        };
+        provider.send({name: 'device', body: update});
+    }, 2000);
+
+});
+
+provider.on("error", function(err) {
+    logger.error("onError", err);
+});
