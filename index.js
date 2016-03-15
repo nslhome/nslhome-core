fcf9cff54e0905dd345a560638f9267d73d8b364
Nick Largent <nicklargent@gmail.com>
2016-03-14 20:39:24 -0400

    added mongo helper class

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ exports = module.exports = nslhome_core = {};
 nslhome_core.Logger = require('./lib/logger');
 nslhome_core.DeviceMangager = require('./lib/device-manager');
 nslhome_core.Provider = require('./lib/provider');
+nslhome_core.Mongo = require('./lib/mongo');
 
 nslhome_core.provider = function(providerType) {
     return new nslhome_core.Provider(providerType);
